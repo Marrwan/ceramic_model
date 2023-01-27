e074fc56db47824771c54eb113235667aa68d7aa
marrwan <abdulbasitdamilola6@gmail.com>
2023-01-27 23:25:51 +0100

    ohoo

--- test.js
+++ test.js
@@ -22,6 +22,7 @@ let arr = []
 let jsonArray = result.split("\n");
 // return res.json(jsonArray)
 jsonArray.splice(-2,2)
+return res.send(jsonArray);
 jsonArray.forEach(jsonString => {
     let parsedData =JSON.parse(jsonString);
 
